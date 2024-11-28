5a27891719957ca464fdf82fc1a3374518ce7946
Md-Rifat-Islam <rifat010bushra@gmail.com>
2024-11-28 19:05:24 +0600

    &apos; updated

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -52,7 +52,7 @@ const Contact = () => {
                     <div className="xl:w-[54%] order-2 xl:oreder-none">
                         <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
                             <h3 className="text-4xl text-accent ">
-                                Let's work together
+                                Let&apos;s work together
                             </h3>
                             <p className="text-white/60">
                                 fdskj jf igjd skj ks df hs fhj odif
